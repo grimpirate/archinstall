5d81e91cbb188f042ff8a1c180727aea86058c2b
grimpirate <grimpirate_jrs@yahoo.com>
2025-10-03 19:33:56 -0400

    Typo fix

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 STEP=$(dialog --title "ARCH LINUX" --menu "Select procedure" 8 45 0 1 "Network setup" 2>&1 >/dev/tty)
 
-case STEP in
-	"1")
+case $STEP in
+	1)
 		curl -fsSL https://github.com/grimpirate/archinstall/raw/refs/heads/main/network.sh | sh
 	;;
 	*)
